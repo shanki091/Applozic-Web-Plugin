5a490b12424aaf6cc718bdc3e6f0d418dbbebb68
Devashish Datt Mamgain <devashish@applozic.com>
2016-07-15 23:54:26 +0530

    Message list

--- message/advanced/js/index.js
+++ message/advanced/js/index.js
@@ -1,6 +1,8 @@
 $('.chat[data-chat=person2]').addClass('active-chat');
 $('.person[data-chat=person2]').addClass('active');
 
+var applozicUI = new ApplozicUI();
+
 $('body').on('mousedown', '.left .person', function(event) {
     if ($(this).hasClass('.active')) {
         return false;
@@ -11,7 +13,34 @@ $('body').on('mousedown', '.left .person', function(event) {
         $('.chat').removeClass('active-chat');
         $('.left .person').removeClass('active');
         $(this).addClass('active');
-        //$('.chat[data-chat = '+findChat+']').addClass('active-chat');
-        //$applozic.fn.applozic('loadTab', findChat);
+        $('.chat[data-chat ="'+findChat+'"]').addClass('active-chat');
+        applozicUI.loadConversation(findChat);
     }
 });
+
+function ApplozicUI() {
+
+  var _this = this;
+  var chatTemplate = '<div class="bubble ${youMeExpr}">${msgTextExpr}</div>';
+
+  _this.loadConversation = function(contactId) {
+      $applozic.fn.applozic('messageList',
+        {
+          'id': contactId,
+          'isGroup': false,
+          'callback': function(response) {
+            //console.log(response);
+            var messages = response.messages;
+            var chats = "";
+            for (var key in messages) {
+              var message = messages[key];
+                chats += chatTemplate.replace("${youMeExpr}", (message.type == 'outbox' ? "you" : "me"))
+                  .replace("${msgTextExpr}", message.message);
+              }
+              $(".chat[data-chat='" + contactId + "']").append(chats);
+          }
+        });
+  }
+
+
+}
